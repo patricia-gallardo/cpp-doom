853ead84ebe1c039da470fc6f043b3c777f78067
Patricia Aas <psmaas@gmail.com>
2022-02-06 16:13:24 +0100

    Add space

--- src/hexen/textdefs.hpp
+++ src/hexen/textdefs.hpp
@@ -21,19 +21,19 @@
 #define PRESSYN		"press y or n."
 #define TXT_PAUSED	"PAUSED"
 #define QUITMSG		"are you sure you want to\nquit this great game?"
-#define LOADNET		"you can't do load while in a net game!\n\n"PRESSKEY
-#define QLOADNET	"you can't quickload during a netgame!\n\n"PRESSKEY
-#define QSAVESPOT	"you haven't picked a quicksave slot yet!\n\n"PRESSKEY
-#define SAVEDEAD 	"you can't save if you aren't playing!\n\n"PRESSKEY
-#define QSPROMPT 	"quicksave over your game named\n\n'%s'?\n\n"PRESSYN
+#define LOADNET		"you can't do load while in a net game!\n\n" PRESSKEY
+#define QLOADNET	"you can't quickload during a netgame!\n\n" PRESSKEY
+#define QSAVESPOT	"you haven't picked a quicksave slot yet!\n\n" PRESSKEY
+#define SAVEDEAD 	"you can't save if you aren't playing!\n\n" PRESSKEY
+#define QSPROMPT 	"quicksave over your game named\n\n'%s'?\n\n" PRESSYN
 #define QLPROMPT	"do you want to quickload the game named"\
-					"\n\n'%s'?\n\n"PRESSYN
+					"\n\n'%s'?\n\n" PRESSYN
 #define NEWGAME		"you can't start a new game\n"\
-					"while in a network game.\n\n"PRESSKEY
+					"while in a network game.\n\n" PRESSKEY
 #define MSGOFF		"Messages OFF"
 #define MSGON		"Messages ON"
-#define NETEND		"you can't end a netgame!\n\n"PRESSKEY
-#define ENDGAME		"are you sure you want to end the game?\n\n"PRESSYN
+#define NETEND		"you can't end a netgame!\n\n" PRESSKEY
+#define ENDGAME		"are you sure you want to end the game?\n\n" PRESSYN
 #define DOSY		"(press y to quit to dos.)"
 #define TXT_GAMMA_LEVEL_OFF	"GAMMA CORRECTION OFF"
 #define TXT_GAMMA_LEVEL_1	"GAMMA CORRECTION LEVEL 1"
--- src/strife/d_englsh.hpp
+++ src/strife/d_englsh.hpp
@@ -37,32 +37,32 @@
 #define PRESSYN 	"press y or n."
 #define QUITMSG	"are you sure you want to\nquit this great game?"
 // [STRIFE] modified:
-#define LOADNET 	"you can't load while in a net game!\n\n"PRESSKEY
-#define QLOADNET	"you can't quickload during a netgame!\n\n"PRESSKEY
+#define LOADNET 	"you can't load while in a net game!\n\n" PRESSKEY
+#define QLOADNET	"you can't quickload during a netgame!\n\n" PRESSKEY
 // [STRIFE] modified:
-#define QSAVESPOT	"you haven't picked a\nquicksave slot yet!\n\n"PRESSKEY
+#define QSAVESPOT	"you haven't picked a\nquicksave slot yet!\n\n" PRESSKEY
 // [STRIFE] modified:
-#define SAVEDEAD 	"you're not playing a game\n\n"PRESSKEY
-#define QSPROMPT 	"quicksave over your game named\n\n'%s'?\n\n"PRESSYN
+#define SAVEDEAD 	"you're not playing a game\n\n" PRESSKEY
+#define QSPROMPT 	"quicksave over your game named\n\n'%s'?\n\n" PRESSYN
 // [STRIFE] modified:
-#define QLPROMPT	"do you want to quickload\n\n'%s'?\n\n"PRESSYN
+#define QLPROMPT	"do you want to quickload\n\n'%s'?\n\n" PRESSYN
 
 #define NEWGAME	\
 "you can't start a new game\n"\
-"while in a network game.\n\n"PRESSKEY
+"while in a network game.\n\n" PRESSKEY
 
 #define NIGHTMARE	\
 "are you sure? this skill level\n"\
-"isn't even remotely fair.\n\n"PRESSYN
+"isn't even remotely fair.\n\n" PRESSYN
 
 #define SWSTRING	\
 "this is the shareware version of doom.\n\n"\
-"you need to order the entire trilogy.\n\n"PRESSKEY
+"you need to order the entire trilogy.\n\n" PRESSKEY
 
 #define MSGOFF	"Messages OFF"
 #define MSGON		"Messages ON"
-#define NETEND	"you can't end a netgame!\n\n"PRESSKEY
-#define ENDGAME	"are you sure you want\nto end the game?\n\n"PRESSYN
+#define NETEND	"you can't end a netgame!\n\n" PRESSKEY
+#define ENDGAME	"are you sure you want\nto end the game?\n\n" PRESSYN
 
 // haleyjd 09/11/10: [STRIFE] No "to dos." on this
 #define DOSY		"(press y to quit)" 
