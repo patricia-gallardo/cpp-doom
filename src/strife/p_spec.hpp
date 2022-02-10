84decce519c7e514cb08b94459a987d84d6b325c
Patricia Aas <patricia@turtlesec.no>
2022-02-10 13:41:46 +0100

    More minimal diff - copy action_hook.hpp from the PR

--- src/strife/p_spec.hpp
+++ src/strife/p_spec.hpp
@@ -130,7 +130,7 @@ bool EV_ClearForceFields(line_t* line);  // villsa [STRIFE]
 //
 // P_LIGHTS
 //
-typedef struct
+typedef struct fireflicker_s
 {
     thinker_t	thinker;
     sector_t*	sector;
@@ -437,7 +437,7 @@ typedef enum
 
 // villsa [STRIFE] Rogue added a second line_t in the struct
 // backsector is removed
-typedef struct
+typedef struct slidedoor_s
 {
     thinker_t   thinker;
     sdt_e       type;
