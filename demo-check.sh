b0644e909c9b1823d22362fbbc3ccff08fa12372
Olafur Waage <olafurw@gmail.com>
2022-02-24 06:17:28 +0100

    Adding very simple demo check for ubuntu.

---
+++ demo-check.sh
@@ -0,0 +1,2 @@
+./build/src/crispy-doom -nographics -nosound -nograbmouse -iwad data/demos/DOOM.WAD -statdump data/demos/compare.txt -playdemo data/demos/m1-simple;
+diff --strip-trailing-cr data/demos/compare.txt data/demos/m1-simple.txt;
