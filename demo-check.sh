c491d439652673e9307d7674aafeb2a347630274
Ólafur Waage <olafurw@gmail.com>
2022-02-24 15:42:49 +0100

    Adding other 2 demo checks.

--- demo-check.sh
+++ demo-check.sh
@@ -1,2 +1,16 @@
 $1/build/src/crispy-doom -nographics -nosound -nograbmouse -iwad data/demos/DOOM.WAD -statdump $1/compare.txt -playdemo data/demos/m1-simple;
-diff --strip-trailing-cr $1/compare.txt data/demos/m1-simple.txt;
+if ! diff --strip-trailing-cr $1/compare.txt data/demos/m1-simple.txt; then
+    exit 1;
+fi
+
+$1/build/src/crispy-doom -nographics -nosound -nograbmouse -iwad data/demos/DOOM.WAD -statdump $1/compare.txt -playdemo data/demos/m1-fast;
+if ! diff --strip-trailing-cr $1/compare.txt data/demos/m1-fast.txt; then
+    exit 1;
+fi
+
+$1/build/src/crispy-doom -nographics -nosound -nograbmouse -iwad data/demos/DOOM.WAD -statdump $1/compare.txt -playdemo data/demos/m1-normal;
+if ! diff --strip-trailing-cr $1/compare.txt data/demos/m1-normal.txt; then
+    exit 1;
+fi
+
+exit 0;
