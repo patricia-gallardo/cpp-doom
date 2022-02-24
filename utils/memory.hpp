dcbb95bc1eb16cb49202770d3b03d2070c89b733
Patricia Aas <patricia@turtlesec.no>
2022-02-24 11:34:42 +0100

    Clang Format of headers in utils

--- utils/memory.hpp
+++ utils/memory.hpp
@@ -6,23 +6,21 @@
 #include "../src/z_zone.hpp"
 
 // todo fix me
-template<typename DataType>
-auto create_struct()
-{
-    auto *mem = malloc(sizeof(DataType));
-    return new (mem) DataType{};
+template <typename DataType>
+auto create_struct() {
+  auto *mem = malloc(sizeof(DataType));
+  return new (mem) DataType {};
 }
 
-template <typename DataType> auto create_struct(const std::size_t size) {
+template <typename DataType>
+auto create_struct(const std::size_t size) {
   auto *mem = malloc(sizeof(DataType) * size);
   return static_cast<DataType *>(new (mem) DataType[size]);
 }
 
-template<typename DataType>
-auto zmalloc(size_t size, int tag, void *ptr)
-{
+template <typename DataType>
+auto zmalloc(size_t size, int tag, void *ptr) {
   return static_cast<DataType>(Z_Malloc(static_cast<int>(size), tag, ptr));
 }
-
 
 #endif // CRISPY_DOOM_MEMORY_HPP
