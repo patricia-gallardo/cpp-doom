8570b1b955e46ef99974e060fa5fee216764326a
Olafur Waage <olafurw@gmail.com>
2022-02-24 06:47:47 +0100

    cwd issues, trying to see what's in the folder.

--- demo-check.sh
+++ demo-check.sh
@@ -1,2 +1,3 @@
+ls -lart build/src/;
 ./build/src/crispy-doom -nographics -nosound -nograbmouse -iwad data/demos/DOOM.WAD -statdump data/demos/compare.txt -playdemo data/demos/m1-simple;
 diff --strip-trailing-cr data/demos/compare.txt data/demos/m1-simple.txt;
