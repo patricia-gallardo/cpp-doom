0c39b47dd9010ccf6142aae9838065271c8fe455
Patricia Aas <patricia@turtlesec.no>
2022-02-24 12:21:32 +0100

    Clang Format of headers in midiproc

--- midiproc/buffer.hpp
+++ midiproc/buffer.hpp
@@ -23,32 +23,31 @@
 #define BUFFER_SIZE 1024
 
 typedef struct {
-    uint8_t   buffer[BUFFER_SIZE]; // Buffer.
-    uint8_t  *buffer_end;          // End of Buffer.
-    uint8_t  *data;                // Start of actual data.
-    uint8_t  *data_end;            // End of actual data.
-    int   data_len;            // Length of actual data.
+  uint8_t  buffer[BUFFER_SIZE]; // Buffer.
+  uint8_t *buffer_end;          // End of Buffer.
+  uint8_t *data;                // Start of actual data.
+  uint8_t *data_end;            // End of actual data.
+  int      data_len;            // Length of actual data.
 } buffer_t;
 
 typedef struct {
-    buffer_t *buffer;
-    uint8_t      *pos;
+  buffer_t *buffer;
+  uint8_t  *pos;
 } buffer_reader_t;
 
 buffer_t *NewBuffer();
-void DeleteBuffer(buffer_t* buf);
-int Buffer_Data(buffer_t *buf, uint8_t  **data);
-bool Buffer_Push(buffer_t *buf, const void *data, int len);
-void Buffer_Shift(buffer_t *buf, int len);
-void Buffer_Clear(buffer_t *buf);
+void      DeleteBuffer(buffer_t *buf);
+int       Buffer_Data(buffer_t *buf, uint8_t **data);
+bool      Buffer_Push(buffer_t *buf, const void *data, int len);
+void      Buffer_Shift(buffer_t *buf, int len);
+void      Buffer_Clear(buffer_t *buf);
 
-buffer_reader_t *NewReader(buffer_t* buffer);
-void DeleteReader(buffer_reader_t *reader);
-int Reader_BytesRead(buffer_reader_t *reader);
-bool Reader_ReadInt8(buffer_reader_t *reader, uint8_t *out);
-bool Reader_ReadInt16(buffer_reader_t *reader, uint16_t *out);
-bool Reader_ReadInt32(buffer_reader_t *reader, uint32_t *out);
-char *Reader_ReadString(buffer_reader_t *reader);
+buffer_reader_t *NewReader(buffer_t *buffer);
+void             DeleteReader(buffer_reader_t *reader);
+int              Reader_BytesRead(buffer_reader_t *reader);
+bool             Reader_ReadInt8(buffer_reader_t *reader, uint8_t *out);
+bool             Reader_ReadInt16(buffer_reader_t *reader, uint16_t *out);
+bool             Reader_ReadInt32(buffer_reader_t *reader, uint32_t *out);
+char            *Reader_ReadString(buffer_reader_t *reader);
 
 #endif
-
