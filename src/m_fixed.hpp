9ddc1ac9973daa2fb81afaf7c68883cd3a57d12c
Patricia Aas <patricia@turtlesec.no>
2022-02-26 00:02:56 +0100

    The Ubuntu 20.04 GCC does not accept auto parameters

--- src/m_fixed.hpp
+++ src/m_fixed.hpp
@@ -24,7 +24,8 @@
 //
 constexpr auto FRACBITS = 16;
 constexpr auto FRACUNIT = (1 << FRACBITS);
-constexpr auto FIXED2DOUBLE(auto x) { return (x / static_cast<double>(FRACUNIT)); }
+template<typename T>
+constexpr auto FIXED2DOUBLE(T x) { return (x / static_cast<double>(FRACUNIT)); }
 
 using fixed_t = int;
 
