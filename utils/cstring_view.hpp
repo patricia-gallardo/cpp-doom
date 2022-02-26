40636598f6745737770e64911338a7eb5877a8c8
Patricia Aas <patricia@turtlesec.no>
2022-02-26 13:13:02 +0100

    Add @cor3ntin's cstring_view

---
+++ utils/cstring_view.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <cstring>
+#include <string>
+
+struct cstring_view {
+
+  cstring_view(const char * str)
+      : d(str)
+      , s(strlen(d)) { }
+  cstring_view(const std::string_view & str)
+      : d(str.data())
+      , s(str.size()) { }
+
+  const char * c_str() const noexcept {
+    return d;
+  }
+
+  const char * data() const noexcept {
+    return d;
+  }
+
+  const char * begin() const noexcept {
+    return d;
+  }
+
+  const char * end() const noexcept {
+    return d + s;
+  }
+
+  size_t size() const noexcept {
+    return s;
+  }
+
+private:
+  const char * d;
+  std::size_t  s;
+};
