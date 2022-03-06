634c077122580ea84b4b022594727bbc4a8b1a70
Patricia Aas <psmaas@gmail.com>
2022-03-06 14:41:47 +0100

    Make tests for M_snprintf

--- test/test_string_functions.cpp
+++ test/test_string_functions.cpp
@@ -2,6 +2,8 @@
 
 #include "m_misc.hpp"
 
+// M_StringJoin
+
 TEST_CASE("Two words", "[strings]") {
   char *      actual   = M_StringJoin("Hello", " World");
   std::string expected = "Hello World";
@@ -22,6 +24,8 @@ TEST_CASE("Path seperator", "[strings]") {
   REQUIRE(expected == actual);
   free(actual);
 }
+
+// M_StringReplace
 
 TEST_CASE("Replace one", "[strings]") {
   std::string haystack    = "Now I know I’ve got a heart, ’cause it’s breaking.";
@@ -61,4 +65,54 @@ TEST_CASE("Replace end", "[strings]") {
   std::string expected    = "Don’t be silly, Toto. Scarecrows don’t bite.";
   REQUIRE(expected == actual);
   free(actual);
+}
+
+// M_snprintf
+
+TEST_CASE("Use format string (short)", "[strings]") {
+  char        actual[50];
+  auto        actual_length = M_snprintf(actual, sizeof(actual), "I, %s, am %d years old.", "Humpty Dumpty", 14);
+  std::string expected      = "I, Humpty Dumpty, am 14 years old.";
+  REQUIRE(34 == actual_length);
+  REQUIRE(expected == actual);
+}
+
+TEST_CASE("Use format string (too long)", "[strings]") {
+  char        actual[24];
+  auto        actual_length = M_snprintf(actual, sizeof(actual), "I, %s, am %d years old.", "Humpty Dumpty", 14);
+  std::string expected      = "I, Humpty Dumpty, am 14";
+  REQUIRE(23 == actual_length);
+  REQUIRE(expected == actual);
+}
+
+TEST_CASE("Use format string (exact)", "[strings]") {
+  char        actual[35];
+  auto        actual_length = M_snprintf(actual, sizeof(actual), "I, %s, am %d years old.", "Humpty Dumpty", 14);
+  std::string expected      = "I, Humpty Dumpty, am 14 years old.";
+  REQUIRE(34 == actual_length);
+  REQUIRE(expected == actual);
+}
+
+TEST_CASE("No format string (short)", "[strings]") {
+  char        actual[50];
+  auto        actual_length = M_snprintf(actual, sizeof(actual), "I, Humpty Dumpty, am 14 years old.");
+  std::string expected      = "I, Humpty Dumpty, am 14 years old.";
+  REQUIRE(34 == actual_length);
+  REQUIRE(expected == actual);
+}
+
+TEST_CASE("No format string (too long)", "[strings]") {
+  char        actual[24];
+  auto        actual_length = M_snprintf(actual, sizeof(actual), "I, Humpty Dumpty, am 14 years old.");
+  std::string expected      = "I, Humpty Dumpty, am 14";
+  REQUIRE(23 == actual_length);
+  REQUIRE(expected == actual);
+}
+
+TEST_CASE("No format string (exact)", "[strings]") {
+  char        actual[35];
+  auto        actual_length = M_snprintf(actual, sizeof(actual), "I, Humpty Dumpty, am 14 years old.");
+  std::string expected      = "I, Humpty Dumpty, am 14 years old.";
+  REQUIRE(34 == actual_length);
+  REQUIRE(expected == actual);
 }
