051693914a4ec3a3381971f4190a29491f419a47
Patricia Aas <psmaas@gmail.com>
2022-03-06 12:12:34 +0100

    Add tests for M_StringReplace

--- test/test_string_functions.cpp
+++ test/test_string_functions.cpp
@@ -22,3 +22,43 @@ TEST_CASE("Path seperator", "[strings]") {
   REQUIRE(expected == actual);
   free(actual);
 }
+
+TEST_CASE("Replace one", "[strings]") {
+  std::string haystack    = "Now I know I’ve got a heart, ’cause it’s breaking.";
+  std::string needle      = "heart";
+  std::string replacement = "build";
+  char *      actual      = M_StringReplace(haystack.c_str(), needle.c_str(), replacement.c_str());
+  std::string expected    = "Now I know I’ve got a build, ’cause it’s breaking.";
+  REQUIRE(expected == actual);
+  free(actual);
+}
+
+TEST_CASE("Replace two", "[strings]") {
+  std::string haystack    = "Put ‘em up! Put ‘em up!";
+  std::string needle      = "up";
+  std::string replacement = "down";
+  char *      actual      = M_StringReplace(haystack.c_str(), needle.c_str(), replacement.c_str());
+  std::string expected    = "Put ‘em down! Put ‘em down!";
+  REQUIRE(expected == actual);
+  free(actual);
+}
+
+TEST_CASE("Replace start", "[strings]") {
+  std::string haystack    = "Here Scarecrow, wanna play ball?";
+  std::string needle      = "Here";
+  std::string replacement = "Hey";
+  char *      actual      = M_StringReplace(haystack.c_str(), needle.c_str(), replacement.c_str());
+  std::string expected    = "Hey Scarecrow, wanna play ball?";
+  REQUIRE(expected == actual);
+  free(actual);
+}
+
+TEST_CASE("Replace end", "[strings]") {
+  std::string haystack    = "Don’t be silly, Toto. Scarecrows don’t talk.";
+  std::string needle      = "talk.";
+  std::string replacement = "bite.";
+  char *      actual      = M_StringReplace(haystack.c_str(), needle.c_str(), replacement.c_str());
+  std::string expected    = "Don’t be silly, Toto. Scarecrows don’t bite.";
+  REQUIRE(expected == actual);
+  free(actual);
+}
