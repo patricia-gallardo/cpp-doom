7d72f16cbcfd5e88fd046c725ac9c24b9616c02f
Olafur Waage <olafurw@gmail.com>
2022-09-06 00:35:46 +0200

    Using cicddemo for demo-check.

--- demo-check.sh
+++ demo-check.sh
@@ -1,14 +1,14 @@
-$1/build/src/crispy-doom -nographics -nosound -nograbmouse -iwad data/demos/DOOM.WAD -statdump $1/compare.txt -playdemo data/demos/m1-simple;
+$1/build/src/crispy-doom -nographics -nosound -nograbmouse -iwad data/demos/DOOM.WAD -statdump $1/compare.txt -cicddemo data/demos/m1-simple;
 if ! diff --strip-trailing-cr $1/compare.txt data/demos/m1-simple.txt; then
     exit 1;
 fi
 
-$1/build/src/crispy-doom -nographics -nosound -nograbmouse -iwad data/demos/DOOM.WAD -statdump $1/compare.txt -playdemo data/demos/m1-fast;
+$1/build/src/crispy-doom -nographics -nosound -nograbmouse -iwad data/demos/DOOM.WAD -statdump $1/compare.txt -cicddemo data/demos/m1-fast;
 if ! diff --strip-trailing-cr $1/compare.txt data/demos/m1-fast.txt; then
     exit 1;
 fi
 
-$1/build/src/crispy-doom -nographics -nosound -nograbmouse -iwad data/demos/DOOM.WAD -statdump $1/compare.txt -playdemo data/demos/m1-normal;
+$1/build/src/crispy-doom -nographics -nosound -nograbmouse -iwad data/demos/DOOM.WAD -statdump $1/compare.txt -cicddemo data/demos/m1-normal;
 if ! diff --strip-trailing-cr $1/compare.txt data/demos/m1-normal.txt; then
     exit 1;
 fi
