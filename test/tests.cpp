15f00a3144066786845eb99665549fde646a1041
Olafur Waage <olafurw@gmail.com>
2022-09-05 22:14:14 +0200

    Catch2 was not linking, missing main, catch_config_main was not working, added main myself for now.

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,7 @@
-#define CATCH_CONFIG_MAIN
-#include <catch.hpp>
+#include <catch2/catch_session.hpp>
+
+int main(int argc, char* argv[])
+{
+	const int result = Catch::Session().run(argc, argv);
+	return result;
+}
